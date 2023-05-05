d5822ecffbc8e43554ec3a5c9a867db958cd4baa
Rashad-Stack <tex.eng.rashed@gmail.com>
2023-05-05 10:33:00 +0600

    Ready to deploy in to heroku

--- server.js
+++ server.js
@@ -11,11 +11,13 @@ const DB = process.env.MONGODB_DATABASE_URL.replace(
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log("DB connection successful!"));
 
-app.listen(process.env.APP_SERVER_PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(process.env.NODE_ENV);
-  console.log(`Server running on  http://127.0.0.1:${process.env.APP_SERVER_PORT}`);
+  console.log(
+    `Server running on  http://127.0.0.1:${process.env.APP_SERVER_PORT}`
+  );
 });
